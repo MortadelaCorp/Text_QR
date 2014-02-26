db4ca2d459c7b6473d6ef3c60dee2e96643eccb8
Josep Sansalvador Balaguer <josp.sb@me.com>
2014-02-26 16:49:05 +0100

    Subiendo contraste

--- Android/Ttqr/src/es/upv/epsa/ti/ttqr/VideoActivity.java
+++ Android/Ttqr/src/es/upv/epsa/ti/ttqr/VideoActivity.java
@@ -10,6 +10,7 @@ import android.content.DialogInterface;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.ColorMatrix;
 import android.graphics.ColorMatrixColorFilter;
 import android.graphics.Paint;
@@ -42,6 +43,7 @@ public class VideoActivity extends Activity implements Camera.PreviewCallback {
 	// Android image data used for displaying the results
 	private Bitmap output;
 	private Bitmap bmp;
+	private Bitmap greyImage;
 	// temporary storage that's needed when converting from BoofCV to Android image data types
 	private byte[] storage;
 
@@ -242,6 +244,59 @@ public class VideoActivity extends Activity implements Camera.PreviewCallback {
 	    return bmpGrayscale;
 	}
 	
+	private Bitmap adjustedContrast(Bitmap src, double value)
+	{
+	    // image size
+	    int width = src.getWidth();
+	    int height = src.getHeight();
+	    // create output bitmap
+
+	    // create a mutable empty bitmap
+	    Bitmap bmOut = Bitmap.createBitmap(width, height, src.getConfig());
+
+	    // create a canvas so that we can draw the bmOut Bitmap from source bitmap
+	    Canvas c = new Canvas();
+	    c.setBitmap(bmOut);
+
+	    // draw bitmap to bmOut from src bitmap so we can modify it
+	    c.drawBitmap(src, 0, 0, new Paint(Color.BLACK));
+
+
+	    // color information
+	    int A, R, G, B;
+	    int pixel;
+	    // get contrast value
+	    double contrast = Math.pow((100 + value) / 100, 2);
+
+	    // scan through all pixels
+	    for(int x = 0; x < width; ++x) {
+	        for(int y = 0; y < height; ++y) {
+	            // get pixel color
+	            pixel = src.getPixel(x, y);
+	            A = Color.alpha(pixel);
+	            // apply filter contrast for every channel R, G, B
+	            R = Color.red(pixel);
+	            R = (int)(((((R / 255.0) - 0.5) * contrast) + 0.5) * 255.0);
+	            if(R < 0) { R = 0; }
+	            else if(R > 255) { R = 255; }
+
+	            /*G = Color.green(pixel);
+	            G = (int)(((((G / 255.0) - 0.5) * contrast) + 0.5) * 255.0);
+	            if(G < 0) { G = 0; }
+	            else if(G > 255) { G = 255; }
+
+	            B = Color.blue(pixel);
+	            B = (int)(((((B / 255.0) - 0.5) * contrast) + 0.5) * 255.0);
+	            if(B < 0) { B = 0; }
+	            else if(B > 255) { B = 255; }*/
+		
+	            // set new pixel color to output bitmap
+	            bmOut.setPixel(x, y, Color.argb(A, R, R, R));
+	        }
+	    }
+	    return bmOut;
+	}
+	
 	/**
 	 * Draws on top of the video stream for visualizing computer vision results
 	 */
@@ -317,12 +372,12 @@ public class VideoActivity extends Activity implements Camera.PreviewCallback {
 
 				// process the most recently converted image by swapping image buffered
 				synchronized (lockGray) {
-
+					greyImage = toGrayscale(bmp);
 				}
 
 				// render the output in a synthetic color image
 				synchronized ( lockOutput ) {
-					output = toGrayscale(bmp);
+					output = adjustedContrast(greyImage, 60);
 				}
 				mDraw.postInvalidate();
 			}

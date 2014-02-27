617c2e266fb877b9cdb8f7829b69597ace8fae00
Andrés Ruiz <ipruch@MacBook-de-Andres.local>
2014-02-27 01:37:57 +0100

    Mucha kk: - Añadido OpenCV para el tratamiento de la imagen. - Quitado y añadido Tesseract mil veces @_@. Al final se supone que la última versión estaba bien, pero para compilar sin errores con el compilador estándar había que añadir libstdc++.6.0.9.dylib a la lista de frameworks/libraries. - Se supone que ya va el OCR con Tesseract y OpenCV. Ahora mismo está configurado para pasarle una imagen capturada por OpenCV al Tesseract cada 3 segundos, y pondrá el texto reconocido en un label.

---
+++ iOS/ocrqr/ocrqr/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ocrqr
+//
+//  Created by Andrés Ruiz on 26/02/14.
+//  Copyright (c) 2014 tratamientodeimagen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
